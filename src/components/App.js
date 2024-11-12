940933b48675e235895e27839e143a5bb1827ed1
RRAJ644 <riturajsingh.17248@gmail.com>
2024-11-12 22:37:52 +0530

    true state

--- src/components/App.js
+++ src/components/App.js
@@ -11,7 +11,7 @@ const App = () => {
       <h1>Parent component</h1>
       <LoginForm
         isLoggedIn={isLoggedIn}
-        onLogin={() => setIsLoggedIn(!isLoggedIn)}
+        onLogin={() => setIsLoggedIn(true)}
       />
     </div>
   )
